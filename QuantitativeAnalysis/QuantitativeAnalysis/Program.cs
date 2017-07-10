4e6c113af87095f8a29dc290b545e072bc689582
heng mao <mdream05301@163.com>
2017-07-10 09:08:46 +0800

    本地修改

--- QuantitativeAnalysis/QuantitativeAnalysis/Program.cs
+++ QuantitativeAnalysis/QuantitativeAnalysis/Program.cs
@@ -10,7 +10,7 @@ namespace QuantitativeAnalysis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
