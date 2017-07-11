459eb0a77680d29ad06724f79fe9ccf6ae49a160
usharei <usharei@163.com>
2017-07-11 17:06:41 +0800

    数据层代码编写

--- QuantitativeAnalysis/QuantitativeAnalysis/Program.cs
+++ QuantitativeAnalysis/QuantitativeAnalysis/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuantitativeAnalysis.DataAccess.Infrastructure;
+using QuantitativeAnalysis.Utilities;
 
 namespace QuantitativeAnalysis
 {
@@ -10,7 +12,12 @@ namespace QuantitativeAnalysis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello 1997！");
+            var connStr = "server=(local);uid=sa;pwd=maoheng0;";
+            var reader = new SqlServerReader(connStr);
+            var dt = reader.GetDataTable("SELECT TOP 1000 * FROM [optionTickData].[dbo].[MarketData_10000001_SH]");
+            //var reader = new WindReader();
+            //var data = reader.GetDailyDataTable("000001.SZ", "open,high,low,close,volume,amt,vwap,adjfactor,trade_status", "2017-06-10".ToDateTime(), "2017-07-09".ToDateTime(), "");
+
         }
     }
 }
