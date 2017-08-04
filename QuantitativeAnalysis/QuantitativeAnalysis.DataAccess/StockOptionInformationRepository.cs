e7e2fc94df98e0ca81e24594c81384071e33e6c9
heng mao <mdream05301@163.com>
2017-08-05 07:35:35 +0800

    更新

---
+++ QuantitativeAnalysis/QuantitativeAnalysis.DataAccess/StockOptionInformationRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuantitativeAnalysis.DataAccess.Infrastructure;
+using System.Configuration;
+using QuantitativeAnalysis.Utilities;
+using System.IO;
+
+namespace QuantitativeAnalysis.DataAccess
+{
+    public class StockOptionInformationRepository
+    {
+        public StockOptionInformationRepository(ConnectionType type)
+        {
+            sqlReader = new SqlServerReader(type);
+            sqlWriter = new SqlServerWriter(type);
+        }
+        private SqlServerReader sqlReader;
+        private SqlServerWriter sqlWriter;
+        private WindReader windReader = new WindReader();
+
+    }
+}
