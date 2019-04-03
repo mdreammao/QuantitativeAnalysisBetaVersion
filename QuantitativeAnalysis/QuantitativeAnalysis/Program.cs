17740581ea2bf97b8c039bc22e0fa3725c6d1779
heng mao <mdream05301@163.com>
2019-04-03 11:35:07 +0800

    同步

--- QuantitativeAnalysis/QuantitativeAnalysis/Program.cs
+++ QuantitativeAnalysis/QuantitativeAnalysis/Program.cs
@@ -74,8 +74,8 @@ namespace QuantitativeAnalysis
             // moving.backtest("000693.SZ", "2010-01-01".ToDateTime(), "2019-03-09".ToDateTime());
             // moving.backtestAllStock("2010-01-01".ToDateTime(), "2019-03-08".ToDateTime());
             //var tmp=stockMinutelyRepo.GetStockTransactionFromLocalSqlByCodeWithRedis("000905.SH", "2018-10-18".ToDateTime(), "2018-10-18".ToDateTime());
-            priceUnusualMoving moving2 = new priceUnusualMoving(stockMinutelyRepo, stockDailyRepo, stockTickRepo, stockInfoRepo);
-            moving2.backtestByIndexCode("000300.SH", "2010-01-01".ToDateTime(), "2019-03-28".ToDateTime());
+            //priceUnusualMoving moving2 = new priceUnusualMoving(stockMinutelyRepo, stockDailyRepo, stockTickRepo, stockInfoRepo);
+            //moving2.backtestByIndexCode("000300.SH", "2010-01-01".ToDateTime(), "2019-03-28".ToDateTime());
             //moving2.backtest("002250.SZ", "2015-04-20".ToDateTime(), "2015-04-24".ToDateTime());
 
 
@@ -101,10 +101,10 @@ namespace QuantitativeAnalysis
             //recordMinute. BulkLoadStockMinuteOrerByCode("399001.SZ", "2007-01-01".ToDateTime(), "2019-03-12".ToDateTime());
             //Monitor.Bond.ConvertibleBond.IntradayMonitor bond = new Monitor.Bond.ConvertibleBond.IntradayMonitor(stockMinutelyRepo, stockDailyRepo, stockTickRepo, dateRepo);
 
-            //StockIndexBonus myBonus = new StockIndexBonus(stockInfoRepo, stockDailyRepo, dateRepo, lastDay);
-            //myBonus.getBonusByIndex("000016.SH");
-            //myBonus.getBonusByIndex("000300.SH");
-            //myBonus.getBonusByIndex("000905.SH");
+            StockIndexBonus myBonus = new StockIndexBonus(stockInfoRepo, stockDailyRepo, dateRepo, lastDay);
+            myBonus.getBonusByIndex("000016.SH");
+            myBonus.getBonusByIndex("000300.SH");
+            myBonus.getBonusByIndex("000905.SH");
 
             //IndexAnalysis indexAnalysis = new IndexAnalysis(dateRepo, "2019-02-11".ToDateTime());
             //indexAnalysis.differ("510180.OF", "000300.SH");
